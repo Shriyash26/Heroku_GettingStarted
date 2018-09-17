a08cedfcf2620316837abac24fe18f06d4e4a052
Shriyash Mahajan <sm2689@njit.edu>
2018-09-16 23:39:58 -0400

    Added Paraghraphs

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig',array('title'=>'HomePage***'));
+  return $app['twig']->render('index.twig',array('title'=>'English Premier League'));
 });
 
 $app->run();
